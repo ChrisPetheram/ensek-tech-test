736c1d72becc54edadc7b0d3b74a5475b1e45ce7
Chris Petheram <chris@everflowtech.co.uk>
2022-07-20 20:06:07 +0100

    Create stub controller from template

---
+++ EnsekTechTest/MeterReadApi/Api/MeterReadingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MeterReadApi.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeterReadingController : ControllerBase
+    {
+        [HttpPost]
+        public void MeterReadingUploads([FromBody] string value)
+        {
+        }
+    }
+}
