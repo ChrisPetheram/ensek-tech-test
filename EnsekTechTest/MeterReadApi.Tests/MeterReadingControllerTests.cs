4e3639fbabf9b86b67221edc19f556ef26164cea
Chris Petheram <chris@everflowtech.co.uk>
2022-07-21 20:03:11 +0100

    Fill Bad Request tests

--- EnsekTechTest/MeterReadApi.Tests/MeterReadingControllerTests.cs
+++ EnsekTechTest/MeterReadApi.Tests/MeterReadingControllerTests.cs
@@ -28,25 +28,45 @@ namespace MeterReadApi.Tests
         }
 
         [TestMethod]
-        public async Task MeterReadingUploads_PassNullFileSet_ReturnsServerError()
+        public async Task MeterReadingUploads_PassNullFileSet_ReturnsBadRequest()
         {
             _controller.ControllerContext.HttpContext = ConfigureFiles(null);
 
             var result = await _controller.MeterReadingUploads();
 
-            result.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
 
         [TestMethod]
         public async Task MeterReadingUploads_PassZeroFiles_ReturnsBadRequest()
         {
-            Assert.Fail();
+            var files = new FormFileCollection();
+            _controller.ControllerContext.HttpContext = ConfigureFiles(files);
+
+            var result = await _controller.MeterReadingUploads();
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
         }
 
         [TestMethod]
         public async Task MeterReadingUpload_PassMultipleFiles_ReturnsBadRequest()
         {
-            Assert.Fail();
+            var files = new FormFileCollection();
+            files.Add(CreateFile("File1"));
+            files.Add(CreateFile("File2"));
+            _controller.ControllerContext.HttpContext = ConfigureFiles(files);
+
+            var result = await _controller.MeterReadingUploads();
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        private FormFile CreateFile(string fileContents)
+        {
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContents));
+            var file = new FormFile(stream, 0, 0, "MeterReadings", "Meter_Readings.csv");
+
+            return file;
         }
 
         private HttpContext ConfigureFiles(IFormFileCollection files)
