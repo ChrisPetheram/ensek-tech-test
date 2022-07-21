27a0b802587f4cf692d0f0c57c830283d524b9c3
Chris Petheram <chris@everflowtech.co.uk>
2022-07-21 19:53:52 +0100

    Add Api tests

--- EnsekTechTest/MeterReadApi.Tests/MeterReadingControllerTests.cs
+++ EnsekTechTest/MeterReadApi.Tests/MeterReadingControllerTests.cs
@@ -1,9 +1,14 @@
-﻿using MeterReadApi.Api;
+﻿using FluentAssertions;
+using MeterReadApi.Api;
 using MeterReadService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,19 +17,24 @@ namespace MeterReadApi.Tests
     [TestClass]
     public class MeterReadingControllerTests
     {
-        private MeterReadingController _controller;
+        private static MeterReadingController _controller;
 
-        [TestInitialize]
-        public void TestInitialize()
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
         {
             var bulkUploadServiceMock = new Mock<MeterReadBulkUpload>();
             _controller = new MeterReadingController(bulkUploadServiceMock.Object);
+            _controller.ControllerContext = new ControllerContext();
         }
 
         [TestMethod]
         public async Task MeterReadingUploads_PassNullFileSet_ReturnsServerError()
         {
-            Assert.Fail();
+            _controller.ControllerContext.HttpContext = ConfigureFiles(null);
+
+            var result = await _controller.MeterReadingUploads();
+
+            result.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
         }
 
         [TestMethod]
@@ -37,6 +47,16 @@ namespace MeterReadApi.Tests
         public async Task MeterReadingUpload_PassMultipleFiles_ReturnsBadRequest()
         {
             Assert.Fail();
+        }
+
+        private HttpContext ConfigureFiles(IFormFileCollection files)
+        {
+            var context = new DefaultHttpContext();
+
+            context.Request.Headers.Add("Content-Type", "multipart/form-data");
+            context.Request.Form = new FormCollection(null, files);
+
+            return context;
         }
 
     }
