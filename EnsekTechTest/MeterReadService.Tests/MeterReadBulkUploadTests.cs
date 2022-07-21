62e1814860cec9a4825004ad0592faed2802638d
Chris Petheram <chris@everflowtech.co.uk>
2022-07-21 19:53:58 +0100

    add service tests

--- EnsekTechTest/MeterReadService.Tests/MeterReadBulkUploadTests.cs
+++ EnsekTechTest/MeterReadService.Tests/MeterReadBulkUploadTests.cs
@@ -1,11 +1,24 @@
+using FluentAssertions;
+using MeterReadService.Services;
+
 namespace MeterReadService.Tests
 {
     [TestClass]
     public class MeterReadBulkUploadTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static MeterReadBulkUpload _service;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
         {
+            _service = new MeterReadBulkUpload();
+        }
+
+        [TestMethod]
+        public void ParseFile_NullStream_ThrowsException()
+        {
+            Action action = () => _service.ParseFile(null);
+            action.Should().Throw<ArgumentNullException>();
         }
     }
 }
