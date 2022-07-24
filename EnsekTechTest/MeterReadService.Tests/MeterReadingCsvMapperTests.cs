43fd76dbe39492751436d4acd8cbe8f5b57a85f1
Chris Petheram <chris@everflowtech.co.uk>
2022-07-24 11:16:27 +0100

    Write mapper tests

---
+++ EnsekTechTest/MeterReadService.Tests/MeterReadingCsvMapperTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using MeterReadService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterReadService.Tests
+{
+    [TestClass]
+    public class MeterReadingCsvMapperTests
+    {
+        private static MeterReadingCsvMapper _mapper;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _mapper = new();
+        }
+
+        [TestMethod]
+        public void NonInt_AccountNumber_ThrowsException()
+        {
+            var input = new List<string> { "Test", "01/01/2022 10:15", "12345" };
+            var a = () => _mapper.ColumnMapping(input);
+
+            a.Should().Throw<FormatException>();
+        }
+
+        [TestMethod]
+        public void BadDateFormat_ThrowsException()
+        {
+            var input = new List<string> { "1234", "41/41/2022 10:15", "12345" };
+            var a = () => _mapper.ColumnMapping(input);
+
+            a.Should().Throw<FormatException>();
+        }
+
+        [TestMethod]
+        public void NonInt_Reading_ThrowsException()
+        {
+            var input = new List<string> { "1234", "01/01/2022 10:15", "Test" };
+            var a = () => _mapper.ColumnMapping(input);
+
+            a.Should().Throw<FormatException>();
+        }
+
+        [TestMethod]
+        public void ValidValues_Returns_MeterReading()
+        {
+            Assert.Fail();
+        }
+    }
+}
