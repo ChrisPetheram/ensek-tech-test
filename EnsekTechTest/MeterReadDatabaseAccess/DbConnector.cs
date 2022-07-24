71760d35ff6c7c1b2ed06824d32ac1a5e0b508a1
Chris Petheram <chris@everflowtech.co.uk>
2022-07-24 16:59:33 +0100

    Create db connection

---
+++ EnsekTechTest/MeterReadDatabaseAccess/DbConnector.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterReadDatabaseAccess
+{
+    public class DbConnector : IDbConnector
+    {
+
+        public string ConnectionString { get; init; }
+
+        public DbConnector(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            var connection = new SqlConnection(ConnectionString);
+
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ EnsekTechTest/MeterReadDatabaseAccess/IDbConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeterReadDatabaseAccess
+{
+    public interface IDbConnector
+    {
+        string ConnectionString { get; init; }
+
+        public IDbConnection GetConnection();
+    }
+}
