ea46a369678253e334ad72529e8452edde014afd
Chris Petheram <chris@everflowtech.co.uk>
2022-07-24 16:59:07 +0100

    Write SPs for db access

---
+++ EnsekTechTest/MetersDatabase/StoredProcedures/InsertMeterReading.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[InsertMeterReading]
+	@AccountId INT,
+	@ReadingTime DATETIME2,
+	@ReadingValue INT
+AS
+BEGIN TRY
+		INSERT INTO [MeterReading] (
+			AccountId, 
+			MeterReadingDateTime, 
+			MeterReadValue)
+		VALUES (
+			@AccountId,
+			@ReadingTime,
+			@ReadingValue
+		);
+
+		RETURN SCOPE_IDENTITY();
+	END TRY
+	BEGIN CATCH
+		RETURN 0
+	END CATCH	
---
+++ EnsekTechTest/MetersDatabase/StoredProcedures/MeterReadingExists.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[MeterReadingExists]
+	@AccountId INT,
+	@ReadingTime DATETIME2,
+	@ReadingValue INT
+AS
+	SELECT COUNT(*) 
+	FROM [MeterReading] 
+	WHERE 
+		[AccountId] = @AccountId
+	AND [MeterReadingDateTime] = @ReadingTime
+	AND [MeterReadValue] = @ReadingValue
