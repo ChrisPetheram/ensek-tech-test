38b8cd652ff4e168e08c23e07778fe0134fb3558
Chris Petheram <chris@everflowtech.co.uk>
2022-07-24 17:16:46 +0100

    Write insert function

--- EnsekTechTest/MeterReadDatabaseAccess/MeterReadingRepository.cs
+++ EnsekTechTest/MeterReadDatabaseAccess/MeterReadingRepository.cs
@@ -1,15 +1,63 @@
-﻿using MeterReadEntities;
+﻿using Dapper;
+using MeterReadEntities;
+using System.Data;
 
 namespace MeterReadDatabaseAccess
 {
     public class MeterReadingRepository
     {
+        private IDbConnector _connector;
 
-
-        public void InsertReading(MeterReading reading)
+        public MeterReadingRepository(IDbConnector connector)
         {
-
+            _connector = connector;
         }
 
+        public bool InsertReading(MeterReading reading)
+        {
+            using (var connection = _connector.GetConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("AccountId", reading.AccountId);
+                parameters.Add("ReadingTime", reading.ReadingTime);
+                parameters.Add("ReadingValue", reading.ReadingValue);
+
+                var itemId = connection.QueryFirstOrDefault<int>(
+                    new CommandDefinition(
+                        "[dbo].[InsertMeterReading]",
+                        parameters,
+                        commandType: CommandType.StoredProcedure
+                        ));
+
+                if (itemId == 0)
+                {
+                    return false;
+                }
+
+                reading.Id = itemId;
+                return true;
+            }
+        }
+
+        public bool EntryExists(MeterReading reading)
+        {
+            using (var connection = _connector.GetConnection())
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("AccountId", reading.AccountId);
+                parameters.Add("ReadingTime", reading.ReadingTime);
+                parameters.Add("ReadingValue", reading.ReadingValue);
+
+                var exists = connection.QueryFirstOrDefault<int>(
+                    new CommandDefinition(
+                        "[dbo].[MeterReadingExists]",
+                        parameters,
+                        commandType: CommandType.StoredProcedure
+                        ));
+
+                return (exists) > 0;
+            }
+            
+        }
     }
 }
