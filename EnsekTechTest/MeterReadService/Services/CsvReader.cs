9a4206fea04670eae66fe57f88f5e4f39933651c
Chris Petheram <chris@everflowtech.co.uk>
2022-07-24 11:36:00 +0100

    First draft of reader

--- EnsekTechTest/MeterReadService/Services/CsvReader.cs
+++ EnsekTechTest/MeterReadService/Services/CsvReader.cs
@@ -9,9 +9,48 @@ namespace MeterReadService.Services
 {
     public class CsvReader<T>
     {
-        public (ICollection<T> successes, ICollection<string> failures) GetRows(Stream file, ICsvMapper<T> csvMapper)
+        private ICsvMapper<T> _mapper;
+
+        public CsvReader(ICsvMapper<T> mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+        }
+
+        public (ICollection<T> successes, ICollection<string> failures) GetRows(Stream file)
+        {
+            if (file == null)
+                throw new ArgumentNullException(nameof(file));
+
+            if (_mapper == null)
+                throw new ArgumentNullException(nameof(_mapper));
+
+            var successes = new List<T>();
+            var failures = new List<string>();
+            string currentLine = null;
+
+            using (var reader = new StreamReader(file))
+            {
+                file.Seek(0, SeekOrigin.Begin);
+
+                while (reader.Peek() >= 0)
+                {
+                    try
+                    {
+                        currentLine = reader.ReadLine();
+                        var values = currentLine?.Split(",", StringSplitOptions.TrimEntries);
+
+                        successes.Add(_mapper.ColumnMapping(values!));
+
+                    }
+                    catch (FormatException fEx)
+                    {
+                        failures.Add(currentLine!);
+                        continue;
+                    }
+                }
+            }
+            
+            return (successes, failures);
         }
     }
 }
